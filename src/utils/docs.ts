93696417abd68d2b5341775461beb9be5496cb70
Felipe D S Lima <felipe@monts.com.br>
2020-12-03 14:00:23 -0300

    remove log

--- src/utils/docs.ts
+++ src/utils/docs.ts
@@ -3,7 +3,6 @@ import matter from 'gray-matter'
 import { join } from 'path'
 
 export function getDocBySlug(slug: string) {
-  console.log(process.cwd())
   const realSlug = slug.replace(/\.md$/, '')
   const docsDirectory = join(process.cwd())
   const fullPath = join(docsDirectory, `${realSlug}.md`)
